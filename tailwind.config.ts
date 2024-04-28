c478bebd84a95bf61350a64611ff8c3e41a7ade8
Admiral-Simo <admiralsimo16@gmail.com>
2024-04-28 22:47:11 +0100

    config

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#020817",
+        light: "#f1f5f9",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), addVariablesForColors],
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+export default config;
